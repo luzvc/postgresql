114a603a4e49a931f6dad155b474aedf898a127d
Thiago Dias <tdtadeu@gmail.com>
2018-09-26 20:45:01 -0300

    Trying to find the error

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -5,7 +5,7 @@ package "apt-transport-https"
 apt_repository "postgresql_org_repository" do
 	uri "https://download.postgresql.org/pub/repos/apt/"
 	components ["main", node[:postgresql][:version]]
-	distribution "#{node["lsb"]["codename"]}-pgdg"
+	distribution "#{node["lsb"]["codename"]}"
 	key node[:postgresql][:apt_gpg_key_uri]
 	cache_rebuild true
 end
