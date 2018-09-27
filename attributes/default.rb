af50f20afee42b9dffb060d4505cddc5f582bd84
Thiago Dias <tdtadeu@gmail.com>
2018-09-26 21:35:43 -0300

    Still trying to figure out wtf is going on

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default[:postgresql][:username] = "deploy"
 default[:postgresql][:password] = nil
 default[:postgresql][:version] = "9.6"
-default[:postgresql][:apt_gpg_key_url] = "https://download.postgresql.org/pub/repos/apt/ACCC4CF8.asc"
+default[:postgresql][:postgresql_org_repo] = "https://download.postgresql.org/pub/repos/apt"
+default[:postgresql][:apt_gpg_key_uri] = "https://download.postgresql.org/pub/repos/apt/ACCC4CF8.asc"
--- recipes/repository.rb
+++ recipes/repository.rb
@@ -3,9 +3,9 @@ include_recipe "apt"
 package "apt-transport-https"
 
 apt_repository "postgresql_org_repository" do
-	uri "https://download.postgresql.org/pub/repos/apt/"
+	uri node[:postgresql][:postgresql_org_repo]
 	components ["main", node[:postgresql][:version]]
-	distribution "#{node["lsb"]["codename"]}"
+	distribution "#{node["lsb"]["codename"]}-pgdg"
 	key node[:postgresql][:apt_gpg_key_uri]
 	cache_rebuild true
 end
